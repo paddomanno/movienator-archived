2db1f723892abe244436482a7fecc36fc2fa4e8a
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-02 14:05:26 +0100

    Docker compose now working

--- movienator_react/src/components/RootPage.tsx
+++ movienator_react/src/components/RootPage.tsx
@@ -1,13 +1,7 @@
-import {Link, Outlet} from "react-router-dom";
-import "../Styles/allStyles.css"
-
 export default function RootPage(){
     return(
         <div className={"rootPage"}>
             Hier ist die Root Page
-            <div id="detail">
-                <Outlet/>
-            </div>
         </div>
     )
 }
