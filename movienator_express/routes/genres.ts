32ac03fb354f37f45b26bdb156bfaf26a281825e
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-07 19:38:55 +0100

    Add extern and intern routes for genres

---
+++ movienator_express/routes/genres.ts
@@ -0,0 +1,29 @@
+import Genre from "../entity/genre";
+
+const expressGenres = require('express');
+const genresRouter = expressGenres.Router();
+
+//Returns all the genres there currently are in the database
+//Movies array is filled
+genresRouter.get("/all", async (req,res)=>{
+    try {
+        const allGenres: Genre[] = await Genre.find({
+            relations: { movies: true },
+        });
+        if (allGenres) {
+            allGenres.sort((a, b) => a.genreName.localeCompare(b.genreName));
+            res.status(200).json({
+                data: allGenres,
+            });
+        } else {
+            res.status(404).json();
+        }
+    } catch (er) {
+        console.log(er);
+        res.status(500).json();
+    }
+})
+
+
+
+module.exports = genresRouter
