e6a741108e5e95ed310f4b89d66553df83d3e39f
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-11 16:41:47 +0100

    Insert the last extern tests

--- movienator_express/_test_/extern.test.ts
+++ movienator_express/_test_/extern.test.ts
@@ -6,6 +6,8 @@ import app from '../app'
 import Movie from "../entity/movie";
 import Actor from "../entity/actor";
 import Genre from "../entity/genre";
+import Review from "../entity/review";
+import {MigrationRevertCommand} from "typeorm/commands/MigrationRevertCommand";
 
 beforeAll(async () => {
     await TestDatabaseManager.getInstance().connectTestDatabase()
@@ -96,14 +98,103 @@ describe("Externtest",() => {
         expect(movies.at(0).movieId).toBeDefined()
         expect(movies.at(0).title).toBeDefined()
         expect(movies.at(0).adultContent).toBeDefined()
-    })
+    },10_000)
 
     it("getRecommendationsForUser",async () => {
+        // 436270 is Black Adam
+        // 724495 is The Woman Kind
+        let movie: Movie = Movie.create({
+            movieId: 436270,
+            title : "Black Adam",
+            adultContent : false,
+            actors : [],
+            genres : [],
+            reviews: []
+        })
+        await movie.save()
+        movie = Movie.create({
+            movieId: 724495,
+            title : "The Woman Kind",
+            adultContent : false,
+            actors : [],
+            genres : [],
+            reviews: []
+        })
+        await movie.save()
+        let review: Review = Review.create({
+            reviewMovieMovieId: 436270,
+            reviewUserUserId: 1,
+            title: 'Bruddaler Film',
+            content: 'Des wars mit dem Review',
+            rating: 5,
+            lastUpdated: new Date('2019-10-11'),
+        })
+        await review.save()
+        review = Review.create({
+            reviewMovieMovieId: 724495,
+            reviewUserUserId: 1,
+            title: 'Bruddaler Film',
+            content: 'Des wars mit dem Review',
+            rating: 5,
+            lastUpdated: new Date('2019-10-11'),
+        })
+        await review.save()
 
+
+        let response = await request(app)
+            .get('/extern/user/1/recommendations')
+        expect(response.statusCode).toBe(200)
+        let movies: Movie[] = response.body.data as Movie[]
+        expect(movies.length).toBeGreaterThanOrEqual(1)
+        expect(movies.length).toBeLessThanOrEqual(20)
+        expect(movies.at(0).actors.length).toBe(0)
+        expect(movies.at(0).genres.length).toBeGreaterThanOrEqual(1)
+        expect(movies.at(0).movieId).toBeDefined()
+        expect(movies.at(0).title).toBeDefined()
+        expect(movies.at(0).adultContent).toBeDefined()
+    })
+
+    it("getRecommendationsForUserWrongId",async () => {
+        // 436270 is Black Adam
+        let response = await request(app)
+            .get('/extern/user/10/recommendations')
+        expect(response.statusCode).toBe(404)
+    })
+
+    it("getRecommendationsForUserNonNumber",async () => {
+        // 436270 is Black Adam
+        let response = await request(app)
+            .get('/extern/user/blabla/recommendations')
+        expect(response.statusCode).toBe(500)
     })
 
     it("getRecommendationsForMovieId",async () => {
+        // 436270 is Black Adam
+        let response = await request(app)
+            .get('/extern/movie/436270/recommendations')
+        expect(response.statusCode).toBe(200)
+        let movies: Movie[] = response.body.data as Movie[]
+        expect(movies.length).toBeGreaterThanOrEqual(1)
+        expect(movies.length).toBeLessThanOrEqual(20)
+        expect(movies.at(0).actors.length).toBe(0)
+        expect(movies.at(0).genres.length).toBeGreaterThanOrEqual(1)
+        expect(movies.at(0).movieId).toBeDefined()
+        expect(movies.at(0).title).toBeDefined()
+        expect(movies.at(0).adultContent).toBeDefined()
+    })
 
+    it("getRecommendationsForMovieWrongId",async () => {
+        // 436270 is Black Adam
+        let response = await request(app)
+            .get('/extern/movie/-1/recommendations')
+        expect(response.statusCode).toBe(500)
+    })
+
+    it("getRecommendationsForMovieNonNumber",async () => {
+        // 436270 is Black Adam
+        let response = await request(app)
+            .get('/extern/movie/blabla/recommendations')
+        expect(response.statusCode).toBe(500)
     })
 
     it("getPopular", async ()=>{
