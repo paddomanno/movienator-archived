339becbc262d8137978000e217850f9ce9a09cd2
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-15 13:59:34 +0100

    Inserts templates for services

---
+++ movienator_react/src/services/UserService.ts
@@ -0,0 +1,9 @@
+import { User } from '../types/User';
+
+export async function getAllUsers(): Promise<User[]> {
+  return [];
+}
+
+export async function getOneUser(userId: number): Promise<User | null> {
+  return null;
+}
