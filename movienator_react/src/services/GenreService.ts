b53c5a3557226258ce54e9ac433ee1e626ec3f80
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-15 21:46:51 +0100

    Insert Genre Service

--- movienator_react/src/services/GenreService.ts
+++ movienator_react/src/services/GenreService.ts
@@ -1,11 +1,17 @@
 import { Genre } from '../types/Genre';
+import { Movie } from '../types/Movie';
+import axios from 'axios';
 
+const baseUrl: string = 'http://localhost:8080/genres';
 export async function getAllGenres(): Promise<Genre[]> {
-  return [];
-}
-
-export async function getOneGenre(
-  genreIdentifier: number | string
-): Promise<Genre | null> {
-  return null;
+  let resArray: Genre[] = [];
+  try {
+    let response = await axios.get(baseUrl + '/all');
+    if (response.status === 200) {
+      resArray = response.data.data as Genre[];
+    }
+  } catch (e) {
+    console.log('Error fetching Genres: ' + e);
+  }
+  return resArray;
 }
