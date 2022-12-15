dd9a2aeb63fb83a0fc16cc6a57cf2e45c8c3adc8
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-15 13:59:34 +0100

    Inserts templates for services

---
+++ movienator_react/src/services/MovieService.ts
@@ -0,0 +1,9 @@
+import { Movie } from '../types/Movie';
+
+export async function getAllMovies(): Promise<Movie[]> {
+  return [];
+}
+
+export async function getOneMovie(movieId: number): Promise<Movie | null> {
+  return null;
+}
