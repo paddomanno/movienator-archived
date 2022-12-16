aa8dc5b897cbb54fc77b32e0ef96c34a551b65cd
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-16 13:35:27 +0100

    Insert User Services

--- movienator_react/src/services/UserService.ts
+++ movienator_react/src/services/UserService.ts
@@ -1,9 +1,213 @@
 import { User } from '../types/User';
+import { Review } from '../types/Review';
+import axios from 'axios';
+import { isNumber } from 'util';
 
+const baseUrl: string = 'http://localhost:8080/users';
 export async function getAllUsers(): Promise<User[]> {
-  return [];
+  let resArray: User[] = [];
+  try {
+    let response = await axios.get(baseUrl + `/all`);
+    if (response.status === 200) {
+      resArray = response.data.data as User[];
+    }
+  } catch (e) {
+    console.log('Error fetching Users: ' + e);
+  }
+  return resArray;
 }
 
-export async function getOneUser(userId: number): Promise<User | null> {
-  return null;
+export async function getOneUser(
+  userIdentifier: number | string
+): Promise<User | null> {
+  let resUser: User | null = null;
+  try {
+    if (isNaN(+userIdentifier)) {
+      let response = await axios.get(
+        baseUrl + `/one/username/${userIdentifier}`
+      );
+      if (response.status === 200) {
+        resUser = response.data.data as User;
+      }
+    } else {
+      let response = await axios.get(baseUrl + `/one/id/${userIdentifier}`);
+      if (response.status === 200) {
+        resUser = response.data.data as User;
+      }
+    }
+  } catch (e) {
+    console.log('Error fetching User: ' + e);
+  }
+  return resUser;
+}
+export async function searchUsersByWord(word: string): Promise<User[]> {
+  let resArray: User[] = [];
+  try {
+    let response = await axios.get(baseUrl + `/username/${word}`);
+    if (response.status === 200) {
+      resArray = response.data.data as User[];
+    }
+  } catch (e) {
+    console.log('Error fetching Users: ' + e);
+  }
+  return resArray;
+}
+export async function getFollowersToUser(userId: number): Promise<User[]> {
+  let resArray: User[] = [];
+  try {
+    let response = await axios.get(baseUrl + `/followers/${userId}`);
+    if (response.status === 200) {
+      resArray = response.data.data as User[];
+    }
+  } catch (e) {
+    console.log('Error fetching Users: ' + e);
+  }
+  return resArray;
+}
+export async function getFollowingToUser(userId: number): Promise<User[]> {
+  let resArray: User[] = [];
+  try {
+    let response = await axios.get(baseUrl + `/following/${userId}`);
+    if (response.status === 200) {
+      resArray = response.data.data as User[];
+    }
+  } catch (e) {
+    console.log('Error fetching Users: ' + e);
+  }
+  return resArray;
+}
+export async function getFollowingThatReviewedMovie(
+  userId: number,
+  movieId: number
+): Promise<User[]> {
+  let resArray: User[] = [];
+  try {
+    let response = await axios.get(
+      baseUrl + `/following/${userId}/rated/${movieId}`
+    );
+    if (response.status === 200) {
+      resArray = response.data.data as User[];
+    }
+  } catch (e) {
+    console.log('Error fetching Users: ' + e);
+  }
+  return resArray;
+}
+export async function getFollowingWithMovieWatchlist(
+  userId: number,
+  movieId: number
+): Promise<User[]> {
+  let resArray: User[] = [];
+  try {
+    let response = await axios.get(
+      baseUrl + `/following/${userId}/watchlist/${movieId}`
+    );
+    if (response.status === 200) {
+      resArray = response.data.data as User[];
+    }
+  } catch (e) {
+    console.log('Error fetching Users: ' + e);
+  }
+  return resArray;
+}
+export async function insertUser(newUser: User): Promise<Boolean> {
+  try {
+    let response = await axios.post(baseUrl, newUser);
+    if (response.status === 201) {
+      return true;
+    }
+  } catch (e) {
+    console.log('Error inserting Users: ' + e);
+  }
+  return false;
+}
+
+/**
+ * User A will start following user B
+ * @param userAId
+ * @param userBId
+ */
+export async function insertFollowing(
+  userAId: number,
+  userBId: number
+): Promise<Boolean> {
+  try {
+    let response = await axios.post(baseUrl + `/follow/${userAId}/${userBId}`);
+    if (response.status === 201) {
+      return true;
+    }
+  } catch (e) {
+    console.log('Error inserting Following: ' + e);
+  }
+  return false;
+}
+export async function putOnWatchlist(
+  userAId: number,
+  movieId: number
+): Promise<Boolean> {
+  try {
+    let response = await axios.post(baseUrl + `/follow/${userAId}/${movieId}`);
+    if (response.status === 201) {
+      return true;
+    }
+  } catch (e) {
+    console.log('Error inserting Watchlist: ' + e);
+  }
+  return false;
+}
+export async function updateUser(user: User): Promise<Boolean> {
+  try {
+    let response = await axios.put(baseUrl, user);
+    if (response.status === 201) {
+      return true;
+    }
+  } catch (e) {
+    console.log('Error inserting User: ' + e);
+  }
+  return false;
+}
+export async function deleteUser(userId: number): Promise<Boolean> {
+  try {
+    let response = await axios.delete(baseUrl + `/${userId}`);
+    if (response.status === 204) {
+      return true;
+    }
+  } catch (e) {
+    console.log('Error inserting User: ' + e);
+  }
+  return false;
+}
+
+/**
+ * User A will stop following User B
+ * @param userIdA
+ * @param userIdB
+ */
+export async function deleteFollowing(
+  userIdA: number,
+  userIdB: number
+): Promise<Boolean> {
+  try {
+    let response = await axios.delete(baseUrl + `/${userIdA}/${userIdB}`);
+    if (response.status === 204) {
+      return true;
+    }
+  } catch (e) {
+    console.log('Error inserting User: ' + e);
+  }
+  return false;
+}
+export async function deleteFromWatchlist(
+  userId: number,
+  movieId: number
+): Promise<Boolean> {
+  try {
+    let response = await axios.delete(baseUrl + `/${userId}/${movieId}`);
+    if (response.status === 204) {
+      return true;
+    }
+  } catch (e) {
+    console.log('Error inserting User: ' + e);
+  }
+  return false;
 }
