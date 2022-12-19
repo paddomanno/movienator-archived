51d4e625c03b70368b4c392601319f8944161e87
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-19 21:37:08 +0100

    Adds movie search page and search bar

--- movienator_react/src/components/MovieSearchBarComponent.tsx
+++ movienator_react/src/components/MovieSearchBarComponent.tsx
@@ -1,3 +1,47 @@
-export default function MovieSearchBarComponent() {
-  return <div>Search Bar</div>;
+import { IconButton, InputBase, Paper } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+export default function MovieSearchBarComponent(props: any) {
+  const [searchWord, setSearchWord] = useState<string | undefined>(props.data);
+  const navigate = useNavigate();
+  function handleChange(e: any) {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setSearchWord(value);
+  }
+
+  function handleClick(e: any) {
+    e.preventDefault();
+    navigate('/search/' + searchWord);
+  }
+  function handlePress(e: any) {
+    if (e.keyCode == 13) {
+      e.preventDefault();
+      navigate('/search/' + searchWord);
+    }
+  }
+
+  return (
+    <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Movie"
+        value={searchWord}
+        onChange={handleChange}
+        onKeyDown={handlePress}
+      />
+      <IconButton
+        type="button"
+        sx={{ p: '10px' }}
+        aria-label="search"
+        onClick={handleClick}
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
 }
