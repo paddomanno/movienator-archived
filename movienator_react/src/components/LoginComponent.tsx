820b59e7288bbcefc156e72c83b875e2c07423fe
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-19 11:36:17 +0100

    Add working login but no cookies

--- movienator_react/src/components/LoginComponent.tsx
+++ movienator_react/src/components/LoginComponent.tsx
@@ -1,3 +1,97 @@
+import { Button, Stack, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { getOneUser } from '../services/UserService';
+import { useNavigate } from 'react-router-dom';
+
+type InputValues = {
+  userName: string;
+  password: string;
+};
 export default function LoginComponent() {
-  return <div>Shows Login Info</div>;
+  const navigate = useNavigate();
+  let defaultValues: InputValues = {
+    userName: '',
+    password: '',
+  };
+  const [formValues, setFormValues] = useState<InputValues>(defaultValues);
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    if (value !== '') {
+      let textField: HTMLElement | null = document.getElementById(
+        `${name}-input`
+      );
+      if (textField != null) {
+        textField.style.backgroundColor = 'white';
+      }
+    }
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    if (formValues.password !== '' && formValues.userName !== '') {
+      getOneUser(formValues.userName).then((user) => {
+        if (user != null) {
+          if (user.password === formValues.password) {
+            //Cockie setzen
+            navigate('/home');
+          } else {
+            let textField: HTMLElement | null =
+              document.getElementById(`password-input`);
+            if (textField != null) {
+              textField.style.backgroundColor = 'red';
+            }
+          }
+        } else {
+          let textField: HTMLElement | null =
+            document.getElementById(`userName-input`);
+          if (textField != null) {
+            textField.style.backgroundColor = 'red';
+          }
+        }
+      });
+    } else {
+      (Object.keys(formValues) as (keyof InputValues)[]).forEach((key) => {
+        if (formValues[key] === '') {
+          let textField: HTMLElement | null = document.getElementById(
+            `${key}-input`
+          );
+          if (textField != null) {
+            textField.style.backgroundColor = 'orange';
+          }
+        }
+      });
+    }
+  }
+
+  return (
+    <>
+      <Stack direction={'column'} spacing={1} alignItems={'center'}>
+        <TextField
+          sx={{ width: '50%' }}
+          id={'userName-input'}
+          name={'userName'}
+          label={'Username'}
+          type={'text'}
+          value={formValues.userName}
+          onChange={handleInputChange}
+        />
+        <TextField
+          sx={{ width: '50%' }}
+          id={'password-input'}
+          name={'password'}
+          label={'password'}
+          type={'text'}
+          value={formValues.password}
+          onChange={handleInputChange}
+        />
+        <Button variant={'contained'} onClick={handleSubmit}>
+          Login
+        </Button>
+      </Stack>
+    </>
+  );
 }
--- movienator_react/src/pages/LoginPage.tsx
+++ movienator_react/src/pages/LoginPage.tsx
@@ -1,4 +1,24 @@
 //Route: movienator3000.com/login
+import { Button, Card, Typography } from '@mui/material';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import LoginComponent from '../components/LoginComponent';
+
 export default function LoginPage() {
-  return <div>Hier ist die Login Page</div>;
+  const navigate = useNavigate();
+  function handleClick(e: any) {
+    e.preventDefault();
+    navigate('/signup');
+  }
+  return (
+    <>
+      <Card>
+        <Typography>Sign into your Account</Typography>
+        <LoginComponent />
+        <Button variant={'contained'} onClick={handleClick}>
+          I don't have an account
+        </Button>
+      </Card>
+    </>
+  );
 }
