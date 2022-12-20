3711124858c73d3890b3b00953a57e2544bec2f4
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-20 09:34:10 +0100

    Adds root bar with links to home, logout and profile

--- movienator_react/src/pages/RootPage.tsx
+++ movienator_react/src/pages/RootPage.tsx
@@ -1,31 +1,79 @@
 //Route: movienator3000.com/
 import { Outlet, useNavigate } from 'react-router-dom';
-import { Button, Typography } from '@mui/material';
+import { Avatar, Button, IconButton, Stack, Typography } from '@mui/material';
 import { useCookies } from 'react-cookie';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import LogoutIcon from '@mui/icons-material/Logout';
+import React, { useEffect, useState } from 'react';
+import { User } from '../types/User';
+import { getOneUser } from '../services/UserService';
 
 export default function RootPage() {
   const navigate = useNavigate();
   const [cookies, setCookie, removeCookie] = useCookies(['userName', 'userId']);
+  const [user, setUser] = useState<User | null>(null);
 
+  useEffect(() => {
+    if (cookies.userId) {
+      getOneUser(cookies.userId).then((res) => {
+        setUser(res);
+      });
+    }
+  }, cookies.userId);
   function handleLogout(e: any) {
     e.preventDefault();
     if (cookies.userName) {
       removeCookie('userName');
       removeCookie('userId');
+      setUser(null);
       navigate('/login');
     }
   }
 
+  function homeClick(e: any) {
+    e.preventDefault();
+    navigate('/home');
+  }
+
+  function profileClick(e: any) {
+    e.preventDefault();
+    navigate('/profile');
+  }
+
   return (
     <div>
-      <div className={'rootPage'}>Hier ist die Root Page</div>
-      {cookies.userName ? (
-        <Button variant={'contained'} onClick={handleLogout}>
-          Log Out
-        </Button>
-      ) : (
-        <Typography>Please log in to use this website</Typography>
-      )}
+      <Stack direction={'row'} justifyContent={'space-between'}>
+        <IconButton onClick={homeClick}>
+          <LiveTvIcon />
+        </IconButton>
+        <Typography variant={'h3'}>Movienator3000</Typography>
+        <Stack direction={'row'} spacing={1}>
+          {user != null ? (
+            <>
+              {user.profileImage != null ? (
+                <IconButton onClick={profileClick}>
+                  <Avatar
+                    alt={user.userName}
+                    src={user.profileImage.ressourceLink}
+                  ></Avatar>
+                </IconButton>
+              ) : (
+                <IconButton onClick={profileClick}>
+                  <Avatar>
+                    {user.firstName.at(0)}
+                    {user.lastName.at(0)}
+                  </Avatar>
+                </IconButton>
+              )}
+              <IconButton onClick={handleLogout}>
+                <LogoutIcon />
+              </IconButton>
+            </>
+          ) : (
+            <Typography>Please log in to use this website</Typography>
+          )}
+        </Stack>
+      </Stack>
       <div id="detail">
         <Outlet />
       </div>
