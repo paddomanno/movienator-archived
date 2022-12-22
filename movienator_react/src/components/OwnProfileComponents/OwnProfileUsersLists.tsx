f436cff024ce0aa27b9fee87b1827bb427d66448
Jankovic, Isidora <isidora.jankovic@stud.h-da.de>
2022-12-22 14:48:26 +0100

    Started UsersList for OwnProfile

--- movienator_react/src/components/OwnProfileComponents/OwnProfileUsersLists.tsx
+++ movienator_react/src/components/OwnProfileComponents/OwnProfileUsersLists.tsx
@@ -1,3 +1,24 @@
-export default function OwnProfileUsersLists(props: any) {
-  return <div>Showing a list of Followers or Following</div>;
+import { Typography, Card } from '@mui/material';
+import { grey } from '@mui/material/colors';
+import { User } from '../../types/User';
+import UsersList from '../ListComponents/UsersList';
+
+type Props = {
+  title: string;
+  users: User[];
+};
+
+export default function OwnProfileUsersLists({ title, users }: Props) {
+  return (
+    <>
+      <Card sx={{ backgroundColor: grey.A200 }}>
+        <Typography variant="h5">{title}</Typography>
+        {users.length != 0 ? (
+          <UsersList users={users} />
+        ) : (
+          <Typography variant="body1">None</Typography>
+        )}
+      </Card>
+    </>
+  );
 }
