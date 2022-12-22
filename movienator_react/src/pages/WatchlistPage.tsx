23072b60febcc8355f3f1b1455bf90e19710bca4
Jankovic, Isidora <isidora.jankovic@stud.h-da.de>
2022-12-22 11:14:30 +0100

    Add WatchlistPage

--- movienator_react/src/pages/WatchlistPage.tsx
+++ movienator_react/src/pages/WatchlistPage.tsx
@@ -1,4 +1,74 @@
 //Route: movienator3000.com/watchlist
+import { useState, useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
+import { getWatchlistMovies } from '../services/MovieService';
+import { Movie } from '../types/Movie';
+import { Button, Card, CardContent, Stack, Typography } from '@mui/material';
+import MoviesList from '../components/ListComponents/MoviesList';
+
 export default function WatchlistPage() {
-  return <div>Hier ist die Watchlist Page</div>;
+  const navigate = useNavigate();
+  const [movies, setMovies] = useState<Movie[] | null>(null);
+  const [cookies] = useCookies(['userName']);
+  const [page, setPage] = useState<number>(1);
+
+  useEffect(() => {
+    if (!cookies.userName) {
+      navigate('/login');
+    }
+    getWatchlistMovies(page).then((movies) => {
+      setMovies(movies);
+    });
+  }, []);
+
+  useEffect(() => {
+    setMovies(null);
+    getWatchlistMovies(page).then((movies) => {
+      setMovies(movies);
+    });
+  }, [page]);
+
+  function decrementPage(e: any) {
+    e.preventDefault();
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  }
+
+  function incrementPage(e: any) {
+    e.preventDefault();
+    setPage(page + 1);
+  }
+
+  return (
+    <Stack direction={'column'} spacing={1}>
+      <Card>
+        <CardContent>
+          <Typography>Your Watchlist</Typography>
+        </CardContent>
+      </Card>
+      {movies != null ? (
+        <>
+          <MoviesList movies={movies} />
+        </>
+      ) : (
+        <>
+          <Typography>Loading...</Typography>
+        </>
+      )}
+      <Card>
+        <CardContent>
+          <Stack direction={'row'} spacing={2}>
+            <Button variant={'contained'} onClick={decrementPage}>
+              Prev Page
+            </Button>
+            <Button variant={'contained'} onClick={incrementPage}>
+              Next Page
+            </Button>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Stack>
+  );
 }
