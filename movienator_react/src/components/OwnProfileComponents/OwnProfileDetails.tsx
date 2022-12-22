30fdecc1246110e4df257f9bbb4763a5f09050d5
Jankovic, Isidora <isidora.jankovic@stud.h-da.de>
2022-12-22 13:56:38 +0100

    Started OwnProfileDetails Component

--- movienator_react/src/components/OwnProfileComponents/OwnProfileDetails.tsx
+++ movienator_react/src/components/OwnProfileComponents/OwnProfileDetails.tsx
@@ -1,3 +1,83 @@
-export default function OwnProfileDetails(props: any) {
-  return <div>Showing Own Details with option to edit</div>;
+import {
+  Avatar,
+  Button,
+  Card,
+  CardContent,
+  Stack,
+  Typography,
+} from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import ShareIcon from '@mui/icons-material/Share';
+import ReviewsIcon from '@mui/icons-material/Reviews';
+import { grey } from '@mui/material/colors';
+import { User } from '../../types/User';
+
+type Props = {
+  user: User;
+};
+
+export default function OwnProfileDetails({ user }: Props) {
+  const SIZE_PROFILEIMAGE = 300;
+
+  return (
+    <Card sx={{ backgroundColor: grey.A200 }}>
+      <CardContent>
+        <Stack direction={'row'} spacing={1} justifyContent={'space-evenly'}>
+          <Card
+            sx={{ backgroundColor: grey.A200 }}
+            style={{ border: 'none', boxShadow: 'none' }}
+          >
+            <>
+              {user.profileImage != null ? (
+                <Avatar
+                  alt={user.firstName + ' ' + user.lastName}
+                  src={user.profileImage.ressourceLink}
+                />
+              ) : (
+                <Avatar
+                  sx={{ width: SIZE_PROFILEIMAGE, height: SIZE_PROFILEIMAGE }}
+                >
+                  {user.firstName.at(0)}
+                  {user.lastName.at(0)}
+                </Avatar>
+              )}
+            </>
+          </Card>
+          <Card
+            sx={{ backgroundColor: grey.A200 }}
+            style={{ border: 'none', boxShadow: 'none' }}
+          >
+            <CardContent>
+              <Typography variant="h5">{user.userName}</Typography>
+              <Typography variant={'body1'}>
+                {user.firstName} {user.lastName}
+              </Typography>
+              <Typography variant={'body1'}>
+                {new Date(user.birthday).getDate()}/
+                {new Date(user.birthday).getMonth()}/
+                {new Date(user.birthday).getFullYear()}
+              </Typography>
+              <Typography variant={'body1'}>{user.comment}</Typography>
+            </CardContent>
+          </Card>
+          <Card
+            sx={{ backgroundColor: grey.A200 }}
+            style={{ border: 'none', boxShadow: 'none' }}
+          >
+            <Stack direction={'column'} spacing={1}>
+              <Button variant="outlined" startIcon={<EditIcon />}>
+                Edit Profile
+              </Button>
+              <Button variant="outlined" startIcon={<ShareIcon />}>
+                Share Profile
+              </Button>
+              {/* <Button variant="outlined" startIcon={<ReviewsIcon />}>
+                My Reviews
+              </Button> */}
+            </Stack>
+          </Card>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
 }
--- movienator_react/src/pages/OwnProfilePage.tsx
+++ movienator_react/src/pages/OwnProfilePage.tsx
@@ -1,4 +1,41 @@
 //Route: movienator3000.com/profile
+import { useState, useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Stack,
+  Typography,
+} from '@mui/material';
+import { User } from '../types/User';
+import { getOneUser } from '../services/UserService';
+import { grey } from '@mui/material/colors';
+import OwnProfileDetails from '../components/OwnProfileComponents/OwnProfileDetails';
+
 export default function OwnProfilePage() {
-  return <div>Hier ist die Own Profile Page</div>;
+  const navigate = useNavigate();
+  const [cookies] = useCookies(['userName']);
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (!cookies.userName) {
+      navigate('/login');
+    }
+    getOneUser(cookies.userName).then((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  return (
+    <Stack direction={'column'} spacing={1}>
+      {user != null ? (
+        <OwnProfileDetails user={user} />
+      ) : (
+        <Typography>Loading...</Typography>
+      )}
+    </Stack>
+  );
 }
