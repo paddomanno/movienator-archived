8f61765b76a4f1f87faca0910d63e59cc74c91ef
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-27 11:54:55 +0100

    Adds List of Reviews on homepage

--- movienator_react/src/components/ListComponents/AllGenresList.tsx
+++ movienator_react/src/components/ListComponents/AllGenresList.tsx
@@ -2,12 +2,13 @@ import { Card, CardContent, Grid, Stack, Typography } from '@mui/material';
 import { Genre } from '../../types/Genre';
 import GenreCard from '../SingleItemComponents/GenreCard';
 import Grid2 from '@mui/material/Unstable_Grid2';
+import { grey } from '@mui/material/colors';
 
 export default function AllGenresList(props: any) {
   const genre: Genre[] = props.data as Genre[];
 
   return (
-    <Card sx={{ backgroundColor: 'lightgrey' }}>
+    <Card sx={{ backgroundColor: grey.A200 }}>
       <CardContent>
         <Typography variant={'subtitle1'}>Browse our Genres</Typography>
         <Grid2 container spacing={0}>
--- movienator_react/src/components/ListComponents/ReviewsList.tsx
+++ movienator_react/src/components/ListComponents/ReviewsList.tsx
@@ -1,3 +1,53 @@
-export default function ReviewsList(props: any) {
-  return <div>1D List of review cards</div>;
+import { Review } from '../../types/Review';
+import { grey } from '@mui/material/colors';
+import {
+  Card,
+  CardContent,
+  IconButton,
+  Stack,
+  Typography,
+} from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2';
+import MovieCard from '../SingleItemComponents/MovieCard';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ReviewCardDetailed from '../SingleItemComponents/ReviewCardDetailed';
+import { useNavigate } from 'react-router-dom';
+
+type Props = {
+  reviews: Review[];
+};
+export default function ReviewsList({ reviews }: Props) {
+  const navigate = useNavigate();
+  function handleClick(e: any) {
+    e.preventDefault();
+    navigate('/followingReviews');
+  }
+
+  return (
+    <Card sx={{ backgroundColor: grey.A200 }}>
+      <CardContent>
+        <Typography>Your friends reviews</Typography>
+        {reviews.length > 0 ? (
+          <Stack direction={'row'} spacing={1} overflow={'auto'}>
+            {reviews.map((review) => (
+              <ReviewCardDetailed
+                showUser={false}
+                showMovie={false}
+                review={review}
+              />
+            ))}
+            <IconButton
+              onClick={handleClick}
+              size={'large'}
+              sx={{ backgroundColor: grey.A400 }}
+            >
+              <ArrowForwardIcon sx={{ width: 100 }} />
+            </IconButton>
+          </Stack>
+        ) : (
+          <Typography>No reviews yet</Typography>
+        )}
+      </CardContent>
+    </Card>
+  );
 }
