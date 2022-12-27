e457e427ed0c1fd634bf76dbab80a5216d195221
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-27 12:41:48 +0100

    Add review list on own user Page

--- movienator_react/src/components/SingleItemComponents/ReviewCardsDetailed/ReviewCardWithMovie.tsx
+++ movienator_react/src/components/SingleItemComponents/ReviewCardsDetailed/ReviewCardWithMovie.tsx
@@ -1,3 +1,13 @@
-export default function ReviewCardWithMovie(props: any) {
-  return <div>Showing Single Review in Profile. Not showing User</div>;
+import { Review } from '../../../types/Review';
+
+type Props = {
+  review: Review;
+};
+export default function ReviewCardWithMovie({ review }: Props) {
+  return (
+    <div>
+      Showing Single Review in Profile. Not showing User{' '}
+      {review.review_movie?.title}
+    </div>
+  );
 }
