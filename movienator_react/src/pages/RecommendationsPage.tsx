d743d7a64039f5341f692dee48d017b2c3d84df9
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-27 10:40:23 +0100

    Adds Recommendations Page

--- movienator_react/src/pages/RecommendationsPage.tsx
+++ movienator_react/src/pages/RecommendationsPage.tsx
@@ -1,4 +1,85 @@
 //Route: movienator3000.com/recommendations
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Movie } from '../types/Movie';
+import { useCookies } from 'react-cookie';
+import { Button, Card, CardContent, Stack, Typography } from '@mui/material';
+import MoviesList from '../components/ListComponents/MoviesList';
+import { getUserRecommendations } from '../services/ExternService';
+
 export default function RecommendationsPage() {
-  return <div>Hier ist die Recommendations Page</div>;
+  const navigate = useNavigate();
+  const [movies, setMovies] = useState<Movie[] | null>(null);
+  const [cookies] = useCookies(['userName', 'userId']);
+  const [page, setPage] = useState<number>(1);
+
+  useEffect(() => {
+    if (!cookies.userName) {
+      navigate('/login');
+    }
+    getUserRecommendations(cookies.userId).then((movies) => {
+      setMovies(movies);
+    });
+  }, []);
+
+  useEffect(() => {
+    setMovies(null);
+    getUserRecommendations(cookies.userId).then((movies) => {
+      setMovies(movies);
+    });
+  }, [page]);
+
+  function decrementPage(e: any) {
+    e.preventDefault();
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  }
+
+  function incrementPage(e: any) {
+    e.preventDefault();
+    setPage(page + 1);
+  }
+
+  return (
+    <Stack direction={'column'} spacing={1}>
+      <Card>
+        <CardContent>
+          <Typography>
+            Your Recommendations. Review more Movies to receive more
+            recommendations.
+          </Typography>
+        </CardContent>
+      </Card>
+      {movies != null ? (
+        <>
+          <MoviesList movies={movies} />
+        </>
+      ) : (
+        <>
+          <Typography>Loading...</Typography>
+        </>
+      )}
+      <Card>
+        <CardContent>
+          <Stack direction={'row'} spacing={2}>
+            <Button
+              variant={'contained'}
+              onClick={decrementPage}
+              disabled={page == 1}
+            >
+              Prev Page
+            </Button>
+            <Button
+              variant={'contained'}
+              onClick={incrementPage}
+              disabled={movies == null || movies.length < 20}
+            >
+              Next Page
+            </Button>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Stack>
+  );
 }
