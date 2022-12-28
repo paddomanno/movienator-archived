4c3f30905a51ae069e9114c6b33f01fe86775a17
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2022-12-28 09:52:28 +0100

    Adds defined Props for most used props

---
+++ movienator_react/src/props/GenreProps.ts
@@ -0,0 +1,9 @@
+import { Genre } from '../types/Genre';
+
+export type SingleGenreProps = {
+  genre: Genre;
+};
+
+export type GenreListProps = {
+  genres: Genre[];
+};
---
+++ movienator_react/src/props/MovieProps.tsx
@@ -0,0 +1,9 @@
+import { Movie } from '../types/Movie';
+
+export type SingleMovieProps = {
+  movie: Movie;
+};
+
+export type MovieListProps = {
+  movies: Movie[];
+};
