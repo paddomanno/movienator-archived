1aad8d680ac94d4c275367e59b3f74682507b02a
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2023-01-08 15:05:59 +0100

    Show "You" when own card is displayed

--- movienator_react/src/components/SingleItemComponents/UserCard.tsx
+++ movienator_react/src/components/SingleItemComponents/UserCard.tsx
@@ -2,14 +2,18 @@ import { Card, CardContent, Stack, Typography } from '@mui/material';
 import { grey } from '@mui/material/colors';
 import OtherUserAvatar from './OtherUserAvatar';
 import { SingleUserProps } from '../../props/UserProps';
+import { useCookies } from 'react-cookie';
 
 export default function UserCard({ user }: SingleUserProps) {
+  const [cookies] = useCookies(['userName', 'userId']);
   return (
     <Card sx={{ backgroundColor: grey.A100 }}>
       <CardContent>
         <Stack direction={'column'} spacing={0} alignItems={'center'}>
           <OtherUserAvatar user={user} />
-          <Typography>{user.userName}</Typography>
+          <Typography>
+            {user.userId == cookies.userId ? 'You' : user.userName}
+          </Typography>
         </Stack>
       </CardContent>
     </Card>
