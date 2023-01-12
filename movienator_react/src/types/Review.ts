14e0a160cbba53b7d2ffd1aad09dbe2e7f801f7b
Kevin Hasse <kevin.gideon.hasse@gmail.com>
2023-01-12 20:24:03 +0100

    Recommendation type in frontend

--- movienator_react/src/types/Review.ts
+++ movienator_react/src/types/Review.ts
@@ -3,18 +3,11 @@ import { User } from './User';
 
 export type Review = {
   reviewMovieMovieId: number;
-
   reviewUserUserId: number;
-
   title: string;
-
   content: string;
-
   rating: number;
-
   lastUpdated: Date;
-
   review_movie: Movie | null;
-
   review_user: User | null;
 };
