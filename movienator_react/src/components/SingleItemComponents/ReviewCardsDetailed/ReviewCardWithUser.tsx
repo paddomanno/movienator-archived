6beaf3c3c1a3b4e51db624b4dbb7b31f37733c1b
Fender, Patrick <patricktfender+git@gmail.com>
2023-01-24 18:43:44 +0100

    eslint fixes ReviewCard components

--- movienator_react/src/components/SingleItemComponents/ReviewCardsDetailed/ReviewCardWithUser.tsx
+++ movienator_react/src/components/SingleItemComponents/ReviewCardsDetailed/ReviewCardWithUser.tsx
@@ -1,34 +1,10 @@
 import { Card, CardContent, Paper, Stack, Typography } from '@mui/material';
 import { grey } from '@mui/material/colors';
-import StarIcon from '@mui/icons-material/Star';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
 import { SingleReviewProps } from '../../../props/ReviewProps';
 import UserCard from '../UserCard';
-import React from 'react';
+import RatingStars from './RatingStars';
 
 export default function ReviewCardWithUser({ review }: SingleReviewProps) {
-  let titleAndDate;
-  let stars;
-  if (review.review_user != null) {
-    titleAndDate = (
-      <Stack direction={'column'}>
-        <Typography variant={'body2'}>
-          {new Date(review.lastUpdated).toDateString()}
-        </Typography>
-        <Typography variant={'body1'}>{review.title}</Typography>
-      </Stack>
-    );
-    stars = (
-      <Stack direction={'row'}>
-        {[...Array(review.rating)].map((x, i) => (
-          <StarIcon />
-        ))}
-        {[...Array(5 - review.rating)].map((x, i) => (
-          <StarBorderIcon />
-        ))}
-      </Stack>
-    );
-  }
   return (
     <Card sx={{ backgroundColor: grey.A200 }}>
       <CardContent>
@@ -48,7 +24,7 @@ export default function ReviewCardWithUser({ review }: SingleReviewProps) {
                 alignItems={'center'}
               >
                 <Typography variant={'h5'}>{review.title}</Typography>
-                {stars}
+                <RatingStars rating={review.rating} max={5} />
               </Stack>
               <Paper
                 sx={{
