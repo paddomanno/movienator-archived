32d68da235ad3618d08a2b1c33bf25e7af3c0468
Fender, Patrick <patricktfender+git@gmail.com>
2023-01-28 12:45:27 +0100

    mock external api calls

--- movienator_express/_test_/extern.test.ts
+++ movienator_express/_test_/extern.test.ts
@@ -14,9 +14,13 @@ import Movie from '../entity/movie';
 import Actor from '../entity/actor';
 import Genre from '../entity/genre';
 import Review from '../entity/review';
+import nock from 'nock';
+import path from 'path';
 
 beforeAll(async () => {
   await TestDatabaseManager.getInstance().connectTestDatabase();
+  nock.back.fixtures = path.join(__dirname, '__nock-fixtures__');
+  nock.back.setMode('lockdown'); // change to 'record' or 'update' when writing new tests or API changes, see https://github.com/nock/nock#modes
 }, 10_000);
 
 afterAll(async () => {
@@ -49,6 +53,9 @@ async function createTestData() {
 describe('Externtest', () => {
   describe('Testing movieSearch Route', () => {
     it('Should return an array of Movies with max length of 20', async () => {
+      const { nockDone } = await nock.back('get-movieSearch-1.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get(
         '/extern/search/movie/Harry?page=1'
       );
@@ -59,8 +66,12 @@ describe('Externtest', () => {
       expect(movies.at(0).movieId).toBeDefined();
       expect(movies.at(0).title).toBeDefined();
       expect(movies.at(0).adultContent).toBeDefined();
+      nockDone();
     });
     it('Should fill the genres array but not actors', async () => {
+      const { nockDone } = await nock.back('get-movieSearch-2.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get(
         '/extern/search/movie/Harry?page=1'
       );
@@ -68,28 +79,40 @@ describe('Externtest', () => {
       const movies: Movie[] = response.body.data as Movie[];
       expect(movies.at(0).actors.length).toBe(0);
       expect(movies.at(0).genres.length).toBeGreaterThanOrEqual(1);
+      nockDone();
     });
   });
 
   describe('Testing actorSearch Route', () => {
     it('Should return an Array of Actors', async () => {
+      const { nockDone } = await nock.back('get-actorSearch-1.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/search/actor/Daniel');
       expect(response.statusCode).toBe(200);
       const actors: Actor[] = response.body.data as Actor[];
       expect(actors.length).toBeGreaterThanOrEqual(1);
       expect(actors.at(0).actorId).toBeDefined();
       expect(actors.at(0).name).toBeDefined();
+      nockDone();
     });
     it('Should not fill the movies array of the actors', async () => {
+      const { nockDone } = await nock.back('get-actorSearch-2.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/search/actor/Daniel');
       expect(response.statusCode).toBe(200);
       const actors: Actor[] = response.body.data as Actor[];
       expect(actors.at(0).movies.length).toBe(0);
+      nockDone();
     });
   });
 
   describe('Testing getActorsToMovie Route', () => {
     it('Should return an array of Actors', async () => {
+      const { nockDone } = await nock.back('get-getActorsToMovie-1.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/actor/movie/436270');
       expect(response.statusCode).toBe(200);
       const actors: Actor[] = response.body.data as Actor[];
@@ -97,16 +120,24 @@ describe('Externtest', () => {
       expect(actors.length).toBeLessThanOrEqual(10);
       expect(actors.at(0).name).toBeDefined();
       expect(actors.at(0).actorId).toBeDefined();
+      nockDone();
     });
 
     it("Should return 500 if the movie ID doesn't exist", async () => {
+      const { nockDone } = await nock.back('get-getActorsToMovie-2.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/actor/movie/xxxx');
       expect(response.statusCode).toBe(500);
+      nockDone();
     });
   });
 
   describe('Testing getMoviesToActor Route', () => {
     it('Should return an Array of Movies', async () => {
+      const { nockDone } = await nock.back('get-getMoviesToActor-1.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/movies/actor/8784');
       expect(response.statusCode).toBe(200);
       const movies: Movie[] = response.body.data as Movie[];
@@ -115,24 +146,36 @@ describe('Externtest', () => {
       expect(movies.at(0).movieId).toBeDefined();
       expect(movies.at(0).title).toBeDefined();
       expect(movies.at(0).adultContent).toBeDefined();
+      nockDone();
     }, 25_000);
 
     it('Should fill the genres array but not the actors array', async () => {
+      const { nockDone } = await nock.back('get-getMoviesToActor-2.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/movies/actor/8784');
       expect(response.statusCode).toBe(200);
       const movies: Movie[] = response.body.data as Movie[];
       expect(movies.at(0).actors.length).toBe(0);
       expect(movies.at(0).genres.length).toBeGreaterThanOrEqual(1);
+      nockDone();
     }, 10_000);
 
     it("Should return 500 if the actors doesn't exist", async () => {
+      const { nockDone } = await nock.back('get-getMoviesToActor-3.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/movies/actor/xxxxx');
       expect(response.statusCode).toBe(500);
+      nockDone();
     }, 10_000);
   });
 
   describe('Testing userRecommendations Route', () => {
     it('Should return an Array of Movies', async () => {
+      const { nockDone } = await nock.back('get-userRecommendations-1.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       // 436270 is Black Adam
       // 724495 is The Woman Kind
       let movie: Movie = Movie.create({
@@ -182,27 +225,39 @@ describe('Externtest', () => {
       expect(movies.at(0).movieId).toBeDefined();
       expect(movies.at(0).title).toBeDefined();
       expect(movies.at(0).adultContent).toBeDefined();
+      nockDone();
     });
 
     it("Should return 404 if the User doesn't exist", async () => {
+      const { nockDone } = await nock.back('get-userRecommendations-2.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       // 436270 is Black Adam
       const response = await request(app).get(
         '/extern/user/10/recommendations'
       );
       expect(response.statusCode).toBe(404);
+      nockDone();
     });
 
     it('Should return 500 if the param is not a number', async () => {
+      const { nockDone } = await nock.back('get-userRecommendations-3.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       // 436270 is Black Adam
       const response = await request(app).get(
         '/extern/user/blabla/recommendations'
       );
       expect(response.statusCode).toBe(500);
+      nockDone();
     });
   });
 
   describe('Testing movieRecommendations', () => {
     it('Should return an array of Movies', async () => {
+      const { nockDone } = await nock.back('get-movieRecommendations-1.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       // 436270 is Black Adam
       const response = await request(app).get(
         '/extern/movie/436270/recommendations'
@@ -216,27 +271,39 @@ describe('Externtest', () => {
       expect(movies.at(0).movieId).toBeDefined();
       expect(movies.at(0).title).toBeDefined();
       expect(movies.at(0).adultContent).toBeDefined();
+      nockDone();
     });
 
     it("Should return 500 if the movie doesn't exist", async () => {
+      const { nockDone } = await nock.back('get-movieRecommendations-2.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       // 436270 is Black Adam
       const response = await request(app).get(
         '/extern/movie/-1/recommendations'
       );
       expect(response.statusCode).toBe(500);
+      nockDone();
     });
 
     it('Should return 500 if the param is not a number', async () => {
+      const { nockDone } = await nock.back('get-movieRecommendations-3.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       // 436270 is Black Adam
       const response = await request(app).get(
         '/extern/movie/blabla/recommendations'
       );
       expect(response.statusCode).toBe(500);
+      nockDone();
     });
   });
 
   describe('Testing getPopular Route', () => {
     it('Should return an array of Movies', async () => {
+      const { nockDone } = await nock.back('get-getPopular-1.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/popular?page=1');
       expect(response.statusCode).toBe(200);
       const movies: Movie[] = response.body.data as Movie[];
@@ -245,30 +312,42 @@ describe('Externtest', () => {
       expect(movies.at(0).movieId).toBeDefined();
       expect(movies.at(0).title).toBeDefined();
       expect(movies.at(0).adultContent).toBeDefined();
+      nockDone();
     });
 
     it('Should fill genres but not actors', async () => {
+      const { nockDone } = await nock.back('get-getPopular-2.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/popular?page=1');
       expect(response.statusCode).toBe(200);
       const movies: Movie[] = response.body.data as Movie[];
       expect(movies.at(0).actors.length).toBe(0);
       expect(movies.at(0).genres.length).toBeGreaterThanOrEqual(1);
+      nockDone();
     });
   });
 
   describe('Testing getAllGenres', () => {
     it('Should return an array of Genres', async () => {
+      const { nockDone } = await nock.back('get-getAllGenres-1.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/genres');
       expect(response.statusCode).toBe(200);
       const genres: Genre[] = response.body.data as Genre[];
       expect(genres.length).toBeGreaterThanOrEqual(1);
       expect(genres.at(0).genreId).toBeDefined();
       expect(genres.at(0).genreName).toBeDefined();
+      nockDone();
     });
   });
 
   describe('Testing getMoviesToGenre Route', () => {
     it('Should return an array of Movies', async () => {
+      const { nockDone } = await nock.back('get-getMoviesToGenre-1.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/movie/genre/28?page=1');
       expect(response.statusCode).toBe(200);
       const movies: Movie[] = response.body.data as Movie[];
@@ -277,39 +356,60 @@ describe('Externtest', () => {
       expect(movies.at(0).movieId).toBeDefined();
       expect(movies.at(0).title).toBeDefined();
       expect(movies.at(0).adultContent).toBeDefined();
+      nockDone();
     });
 
     it('Should fill the genres array but not actors', async () => {
+      const { nockDone } = await nock.back('get-getMoviesToGenre-2.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/movie/genre/28?page=1');
       expect(response.statusCode).toBe(200);
       const movies: Movie[] = response.body.data as Movie[];
       expect(movies.at(0).actors.length).toBe(0);
       expect(movies.at(0).genres.length).toBeGreaterThanOrEqual(1);
+      nockDone();
     });
 
     it('Should return 500 if the param is not a number', async () => {
+      const { nockDone } = await nock.back('get-getMoviesToGenre-3.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/movie/genre/xxx?page=1');
       expect(response.statusCode).toBe(500);
+      nockDone();
     });
 
     it("Should return 500 if the genre doesn't exist", async () => {
+      const { nockDone } = await nock.back('get-getMoviesToGenre-4.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get(
         '/extern/movie/genre/99999?page=1'
       );
       expect(response.statusCode).toBe(404);
+      nockDone();
     });
   });
 
   describe('Testing the hatespeech API', () => {
     it('should return false if profanity was detected', async () => {
+      const { nockDone } = await nock.back('get-hatespeech-1.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/hatespeech?text=FUCK');
       expect(response.statusCode).toBe(200);
       expect(response.body.data as boolean).toBeTruthy();
+      nockDone();
     });
     it('should return false if no profanity was detected', async () => {
+      const { nockDone } = await nock.back('get-hatespeech-2.json');
+      nock.enableNetConnect(/(localhost|127\.0\.0\.1)/); // nock disables external requests, this enables localhost connections
+
       const response = await request(app).get('/extern/hatespeech?text=HELLO');
       expect(response.statusCode).toBe(200);
       expect(response.body.data as boolean).toBeFalsy();
+      nockDone();
     });
   });
 });
