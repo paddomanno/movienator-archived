83be63dfaad1974fafa92a88df7dbc1586c398f6
Fender, Patrick <patricktfender+git@gmail.com>
2023-02-06 15:53:19 +0100

    covered use case userFollow

--- movienator_react/src/_test_/userFollow.test.tsx
+++ movienator_react/src/_test_/userFollow.test.tsx
@@ -1,8 +1,17 @@
-import { act, render, screen, waitFor } from '@testing-library/react';
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import { BrowserRouter } from 'react-router-dom';
-import { getOneUserToUserId } from '../services/UserService';
+import {
+  getOneUserToUserId,
+  insertFollowingToUserIdAndUserId,
+} from '../services/UserService';
 import { User } from '../types/User';
 import OtherProfilePage from '../pages/OtherProfilePage';
 import {
@@ -14,6 +23,16 @@ import { getAllReviewsToUserId } from '../services/ReviewService';
 const fakeLoggedInUser = {
   userName: 'fakeName',
   userId: 1234,
+  firstName: 'Loggedin',
+  lastName: 'Fakeuser',
+  password: 'abc',
+  comment: '',
+  birthday: new Date(),
+  profileImage: null,
+  reviews: [],
+  following: [],
+  followers: [],
+  watchlist: [],
 };
 const fakeViewedUser: User = {
   userId: 5678,
@@ -51,11 +70,6 @@ jest.mock('react-cookie', () => ({
 
 describe('test follow another user', () => {
   test('renders the other user page', async () => {
-    /*let cookies = useCookies(['userName', 'userId']);
-    cookies.set('userName', "Hallo")
-    cookies['userName'] = ('userName', user.userName, { path: '/' });
-    setCookie('userId', user.userId, { path: '/' });*/
-    // (getOneUserToUserId as jest.Mock).mockResolvedValue(fakeLoggedInUser);
     (getOneUserToUserId as jest.Mock).mockImplementation((id) => {
       if (id === fakeLoggedInUser.userId || id === fakeLoggedInUser.userName) {
         return Promise.resolve(fakeLoggedInUser);
@@ -85,108 +99,55 @@ describe('test follow another user', () => {
     });
   });
 
-  // test('writes into input field Username and Password', async () => {
-  //   // arrange
-  //   render(
-  //     <BrowserRouter>
-  //       <LoginPage />
-  //     </BrowserRouter>
-  //   );
+  test('clicking follow updates the user in backend', async () => {
+    // (getOneUserToUserId as jest.Mock).mockImplementation((id) => {
+    //   if (id === fakeLoggedInUser.userId || id === fakeLoggedInUser.userName) {
+    //     return Promise.resolve(fakeLoggedInUser);
+    //   } else {
+    //     return Promise.resolve(fakeViewedUser);
+    //   }
+    // });
+    (getOneUserToUserId as jest.Mock).mockResolvedValueOnce(fakeLoggedInUser);
+    (getOneUserToUserId as jest.Mock).mockResolvedValueOnce(fakeViewedUser);
+    (getOneUserToUserId as jest.Mock).mockResolvedValue({
+      ...fakeViewedUser,
+      followers: [fakeLoggedInUser],
+    });
+    (getAllReviewsToUserId as jest.Mock).mockResolvedValue([]);
+    (getMutualWatchlistToTwoUserIds as jest.Mock).mockResolvedValue([]);
+    (getMutualReviewedToTwoUserIds as jest.Mock).mockResolvedValue([]);
 
-  //   // act
-  //   const inputName = screen.getByLabelText(/username/i) as HTMLInputElement;
-  //   const inputPwd = screen.getByLabelText(/password/i) as HTMLInputElement;
-  //   fireEvent.change(inputName, { target: { value: 'MyUser123' } });
-  //   fireEvent.change(inputPwd, { target: { value: 'root' } });
+    // arrange
+    act(() => {
+      render(
+        <BrowserRouter>
+          <OtherProfilePage />
+        </BrowserRouter>
+      );
+    });
 
-  //   // assert
-  //   expect(inputName.value).toBe('MyUser123');
-  //   expect(inputPwd.value).toBe('root');
-  // });
+    // act
+    await waitFor(() => {
+      expect(screen.getByText(fakeViewedUser.userName)).toBeInTheDocument();
+      expect(
+        screen.getByRole('button', { name: /follow/i })
+      ).toBeInTheDocument();
+    });
+    await act(async () => {
+      fireEvent.click(screen.getByRole('button', { name: 'Follow' }));
+    });
 
-  // test('shows orange input fields when not filled after login button clicked', () => {
-  //   // arrange
-  //   render(
-  //     <BrowserRouter>
-  //       <LoginPage />
-  //     </BrowserRouter>
-  //   );
-
-  //   // act
-  //   const buttonLogin = screen.getByRole(/button/i, { name: 'Login' });
-  //   fireEvent.click(buttonLogin);
-
-  //   // assert
-  //   const inputName = screen.getByLabelText(/username/i) as HTMLInputElement;
-  //   const inputPwd = screen.getByLabelText(/password/i) as HTMLInputElement;
-  //   expect(inputName.style.backgroundColor).toBe('orange');
-  //   expect(inputPwd.style.backgroundColor).toBe('orange');
-  // });
-
-  // test('Login form submits correctly, sets cookies and navigates to HomePage', async () => {
-  //   const loggedInUserDetails = {
-  //     userId: 123,
-  //     userName: 'fakeUser',
-  //     password: 'fakePass',
-  //   };
-
-  //   const loggedInUserFull: User = {
-  //     userId: loggedInUserDetails.userId,
-  //     firstName: '',
-  //     lastName: '',
-  //     userName: loggedInUserDetails.userName,
-  //     password: loggedInUserDetails.password,
-  //     comment: '',
-  //     birthday: new Date(),
-  //     profileImage: null,
-  //     reviews: [],
-  //     following: [],
-  //     followers: [],
-  //     watchlist: [],
-  //   };
-  //   (getOneUserToUserId as jest.Mock).mockResolvedValue(loggedInUserFull);
-
-  //   // arrange
-  //   render(
-  //     <BrowserRouter>
-  //       <LoginPage />
-  //     </BrowserRouter>
-  //   );
-
-  //   // act
-  //   act(() => {
-  //     fireEvent.change(screen.getByLabelText(/username/i), {
-  //       target: { value: loggedInUserDetails.userName },
-  //     });
-  //     fireEvent.change(screen.getByLabelText(/password/i), {
-  //       target: { value: loggedInUserDetails.password },
-  //     });
-  //   });
-
-  //   console.log('## CLICKING LOGIN');
-  //   await act(async () => {
-  //     fireEvent.click(screen.getByText('Login'));
-  //   });
-  //   console.log((getOneUserToUserId as jest.Mock).mock.calls);
-
-  //   // assert
-  //   expect(getOneUserToUserId as jest.Mock).toHaveBeenCalledWith(
-  //     loggedInUserDetails.userName
-  //   );
-  //   expect(mockSetCookie).toHaveBeenCalledWith(
-  //     'userName',
-  //     loggedInUserDetails.userName,
-  //     {
-  //       path: '/',
-  //     }
-  //   );
-  //   expect(mockSetCookie).toHaveBeenCalledWith(
-  //     'userId',
-  //     loggedInUserDetails.userId,
-  //     {
-  //       path: '/',
-  //     }
-  //   );
-  //   expect(mockNavigate).toHaveBeenCalledWith('/home');
-  // });
+    // assert
+    await waitFor(() => {
+      expect(
+        screen.getByRole('button', { name: /unfollow/i })
+      ).toBeInTheDocument();
+      expect(insertFollowingToUserIdAndUserId).toHaveBeenCalledWith(
+        fakeLoggedInUser.userId,
+        fakeViewedUser.userId
+      );
+      expect(screen.getByText('You')).toBeInTheDocument();
+      screen.debug();
+    });
+  });
 });
